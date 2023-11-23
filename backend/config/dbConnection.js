be777644169e3a8f804cbe05b643ab6b20dc8285
Nitish Kumar <118646662+DEVnitishOfficial@users.noreply.github.com>
2023-11-23 19:26:42 +0530

    Update dbConnection.js

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -5,13 +5,13 @@ mongoose.set("strictQuery", false); // disabling strict features of mongoose
 // database connection
 const connectToDB = async () => {
   try {
-    const {connection} = await mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/client_1");
+    const {connection} = await mongoose.connect(process.env.MONGO_URL");
   
     if(connection){
       console.log(`Connected to MongodDB at : ${connection.host}`)
     }
   } catch (error) {
-    console.log(error)
+    console.log('db connection error',error)
     process.exit(1)
   }
 
